798382ef3ef33bb5ed3ceff440a9068f0e59e876
mjilani98 <mjilani@emich.edu>
2025-03-30 12:15:16 -0400

    Created game controller

--- app/src/main/java/com/example/hw4q2/MainActivity.java
+++ app/src/main/java/com/example/hw4q2/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.hw4q2;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,15 +14,74 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    //App Interface
+    private AppInterface appInterface ;
+
+    //Game
+    private Game game;
+
+    //Initialize gesture detector
+    private GestureDetector gestureDetector;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        //Initializing AppInterface
+        appInterface = new AppInterface(this);
+
+        //Initialize game
+        game = new Game();
+
+        //display screen
+        setContentView(appInterface);
+
+        //create a touch handler and initialize gesture detector
+        TouchHandler handler = new TouchHandler();
+        gestureDetector = new GestureDetector(this,handler);
+
+        //display initial game
+        appInterface.showArray(game.getArray(),game.getWindowLocation(),game.getMessage());
+
+    }
+
+    //Event handler of touch event
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        //pass touch event to gesture detector
+        if(!game.gameOver())
+            gestureDetector.onTouchEvent(event);
+
+        return true;
+    }
+
+    //class handles touch event
+    private class TouchHandler extends GestureDetector.SimpleOnGestureListener
+    {
+
+        public boolean onSingleTapConfirmed(MotionEvent event)
+        {
+            //make a move
+            game.move();
+
+            //update screen
+            appInterface.showArray(game.getArray(),game.getWindowLocation(),game.getMessage());
+
+            return true;
+        }
+
+        //on double tap ,swap two numbers
+        public boolean onDoubleTap(MotionEvent event)
+        {
+            //make a swap
+            game.swap();
+
+            //update screen
+            appInterface.showArray(game.getArray(),game.getWindowLocation(),game.getMessage());
+
+            return true;
+        }
     }
 }
